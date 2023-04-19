10f8e5c5d8a0c26c400e60c98f956cd47db08eb4
nyamvularinga <nyamvularinga@gmail.com>
2023-04-19 17:55:29 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -3,4 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+function NavBar() {
+    const links = ["home", "about", "projects"];
+  
+    return <nav>{/* display an <a> tag for each link here */}</nav>;
+    return (
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+      </nav>
+    );
+  }
+  
+  export default NavBar;
+
 ReactDOM.render(<App />, document.getElementById("root"));
